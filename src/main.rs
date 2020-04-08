ebb2c0a75900a50c7b2c277d7c9a2c21184d8dc6
Sean Li <seanli646@gmail.com>
2020-04-08 16:01:54 -0400

    confirms overwrite

--- src/main.rs
+++ src/main.rs
@@ -35,5 +35,23 @@ async fn main() {
     } else if res == Some(true) {
         println!("SEEDING");
         t.serve(false).await;
+    } else {
+        let mut done = false;
+        while !done {
+            println!("Will overwrite some files.  Continue? (y/n)");
+            let mut input = String::new();
+
+            if let Ok(_) = std::io::stdin().read_line(&mut input) {
+                let s = input.to_ascii_lowercase();
+                let s = s.trim();
+
+                if s == "y" || s == "yes" {
+                    t.serve(true).await;
+                    done = true;
+                } else if s == "n" || s == "no" {
+                    done = true;
+                }
+            }
+        }
     }
 }
